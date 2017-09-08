0a12d4515294ca114ebef83d6828d78bc9e508e7
Yana Wilkins <yav9774@gmail.com>
2017-09-08 16:36:28 -0400

    Done-ish

--- Program.cs
+++ Program.cs
@@ -26,6 +26,36 @@ namespace CardCatalog
         library.Catalog.Add(book1); 
         library.Catalog.Add(book2); 
 
+        var titleSeach = library.searchByTitle("Treasure"); 
+        foreach (var item in titleSeach)
+        {
+            Console.WriteLine ("Searching for the title");
+            Console.WriteLine ($"{item.BookName} is written by {item.Author}"); 
+        }
+
+        var authorSearch = library.searchByAuthor("Billy");
+        foreach (var item in authorSearch)
+        {
+            Console.Write ($"{item.Author} wrote {item.BookName}"); 
+        }
+
+
+        var isla = new Member
+        {
+            Name = "Isla",
+            Address = "123 Abc Rd", 
+            PhoneNumber = "123-456-7890", 
+            Email = "awesome@dog.com"
+        }; 
+
+        var overdueSearch = library.searchForOverdue();
+        foreach (var item in overdueSearch)
+        {
+           Console.WriteLine($"{item.BookName} is overdue");
+        }
+        library.CheckOut ("Treasure", "Isla");
+        library.CheckOut ("Norwitch", "Isla"); 
+        Library.CheckInBook ("Treasure","Isla"); 
         }
     }
 }
